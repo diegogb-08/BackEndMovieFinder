ac19c7d6a2110cfe0f313c0b64daf214d994fb7a
Diego <d.garciabrisa@gmail.com>
2021-02-18 20:49:50 +0100

    filmController created, pending update & delete methods

---
+++ storage/filmDb.js
@@ -0,0 +1,18 @@
+class Storage {
+
+    state=[];
+
+    constructor(){
+
+    }
+
+    async set(film){
+        return this.state.push(film);
+    }
+
+    async get(){
+        return this.state;
+    }
+}
+
+module.exports = Storage;
